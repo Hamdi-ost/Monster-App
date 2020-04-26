4a86ec56b07ea242fa8386eb1d145ac1d3e2fb96
Hamdi <benyaflahhamdi2@gmail.com>
2020-04-26 02:19:06 -0400

    Updates

---
+++ precache-manifest.500a04a1ce511d27082fbc9d91909bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70f9e5869c0a5194e93f7226416fb9d8",
+    "url": "/Monster-App/index.html"
+  },
+  {
+    "revision": "08a0d770946ce20d7618",
+    "url": "/Monster-App/static/css/main.db836a00.chunk.css"
+  },
+  {
+    "revision": "c17b3112af99f9a6368d",
+    "url": "/Monster-App/static/js/2.187272e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-App/static/js/2.187272e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08a0d770946ce20d7618",
+    "url": "/Monster-App/static/js/main.b48174a6.chunk.js"
+  },
+  {
+    "revision": "d1056ce9a449b11c12a0",
+    "url": "/Monster-App/static/js/runtime-main.247e4423.js"
+  }
+]);
